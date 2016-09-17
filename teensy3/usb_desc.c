93fad9ed47e4a6b404dd0fb117fa6a6dedd8e87d
Joel Bradshaw <cincodenada@gmail.com>
2016-09-16 23:07:57 -0700

    Fix some terrifying merge hell in usb_desc
    I know I merged this properly at one point, but it got lost somewhere,
    it seems

--- teensy3/usb_desc.c
+++ teensy3/usb_desc.c
@@ -266,42 +266,98 @@ static uint8_t joystick_report_desc[] = {
 
 // This is identical to JOYSTICK_INTERFACE (besides the variable name)
 // but I'd like to keep the MultiJoy code as separate as possible for now
-// https://forum.pjrc.com/threads/32331-USB-HID-Touchscreen-support-needed
-// https://msdn.microsoft.com/en-us/library/windows/hardware/jj151563%28v=vs.85%29.aspx
-// https://msdn.microsoft.com/en-us/library/windows/hardware/jj151565%28v=vs.85%29.aspx
-// https://msdn.microsoft.com/en-us/library/windows/hardware/ff553734%28v=vs.85%29.aspx
-// https://msdn.microsoft.com/en-us/library/windows/hardware/jj151564%28v=vs.85%29.aspx
-// download.microsoft.com/download/a/d/f/adf1347d-08dc-41a4-9084-623b1194d4b2/digitizerdrvs_touch.docx
-        0x09, 0x04,                     // Usage (Touch Screen)
-        0x09, 0x22,                     //   Usage (Finger)
-        0xA1, 0x02,                     //   Collection (Logical)
-        0x09, 0x42,                     //     Usage (Tip Switch)
-        0x15, 0x00,                     //     Logical Minimum (0)
-        0x25, 0x01,                     //     Logical Maximum (1)
-        0x75, 0x01,                     //     Report Size (1)
+#ifdef MULTIJOY_INTERFACE
+static uint8_t multijoy_report_desc[] = {
+        0x05, 0x01,                     // Usage Page (Generic Desktop)
+        0x09, 0x04,                     // Usage (Joystick)
+        0xA1, 0x01,                     // Collection (Application)
+        0x15, 0x00,                     // Logical Minimum (0)
+        0x25, 0x01,                     // Logical Maximum (1)
+        0x75, 0x01,                     // Report Size (1)
+        0x95, 0x20,                     // Report Count (32)
         0x05, 0x09,                     // Usage Page (Button)
         0x19, 0x01,                     // Usage Minimum (Button #1)
         0x29, 0x20,                     // Usage Maximum (Button #32)
-        0x81, 0x02,                     //     Input (variable,absolute)
+        0x81, 0x02,                     // Input (variable,absolute)
+        0x15, 0x00,                     // Logical Minimum (0)
+        0x25, 0x07,                     // Logical Maximum (7)
         0x35, 0x00,                     // Physical Minimum (0)
         0x46, 0x3B, 0x01,               // Physical Maximum (315)
-        0x95, 0x01,                     //     Report Count (1)
-        0x81, 0x02,                     //     Input (variable,absolute)
-        0x26, 0xFF, 0x00,               //     Logical Maximum (255)
-        0x05, 0x01,                     //     Usage Page (Generic Desktop)
+        0x75, 0x04,                     // Report Size (4)
+        0x95, 0x01,                     // Report Count (1)
+        0x65, 0x14,                     // Unit (20)
+        0x05, 0x01,                     // Usage Page (Generic Desktop)
+        0x09, 0x39,                     // Usage (Hat switch)
+        0x81, 0x42,                     // Input (variable,absolute,null_state)
+        0x05, 0x01,                     // Usage Page (Generic Desktop)
         0x09, 0x01,                     // Usage (Pointer)
         0xA1, 0x00,                     // Collection ()
         0x15, 0x00,                     //   Logical Minimum (0)
         0x26, 0xFF, 0x03,               //   Logical Maximum (1023)
         0x75, 0x0A,                     //   Report Size (10)
         0x95, 0x04,                     //   Report Count (4)
+        0x09, 0x30,                     //   Usage (X)
+        0x09, 0x31,                     //   Usage (Y)
+        0x09, 0x32,                     //   Usage (Z)
+        0x09, 0x35,                     //   Usage (Rz)
+        0x81, 0x02,                     //   Input (variable,absolute)
+        0xC0,                           // End Collection
+        0x15, 0x00,                     // Logical Minimum (0)
+        0x26, 0xFF, 0x03,               // Logical Maximum (1023)
+        0x75, 0x0A,                     // Report Size (10)
+        0x95, 0x02,                     // Report Count (2)
+        0x09, 0x36,                     // Usage (Slider)
+        0x09, 0x36,                     // Usage (Slider)
+        0x81, 0x02,                     // Input (variable,absolute)
+        0xC0                            // End Collection
+};
+#endif
+
+#ifdef MULTITOUCH_INTERFACE
+// https://forum.pjrc.com/threads/32331-USB-HID-Touchscreen-support-needed
+// https://msdn.microsoft.com/en-us/library/windows/hardware/jj151563%28v=vs.85%29.aspx
+// https://msdn.microsoft.com/en-us/library/windows/hardware/jj151565%28v=vs.85%29.aspx
+// https://msdn.microsoft.com/en-us/library/windows/hardware/ff553734%28v=vs.85%29.aspx
+// https://msdn.microsoft.com/en-us/library/windows/hardware/jj151564%28v=vs.85%29.aspx
+// download.microsoft.com/download/a/d/f/adf1347d-08dc-41a4-9084-623b1194d4b2/digitizerdrvs_touch.docx
+static uint8_t multitouch_report_desc[] = {
+        0x05, 0x0D,                     // Usage Page (Digitizer)
+        0x09, 0x04,                     // Usage (Touch Screen)
+        0xa1, 0x01,                     // Collection (Application)
+        0x09, 0x22,                     //   Usage (Finger)
+        0xA1, 0x02,                     //   Collection (Logical)
+        0x09, 0x42,                     //     Usage (Tip Switch)
+        0x15, 0x00,                     //     Logical Minimum (0)
+        0x25, 0x01,                     //     Logical Maximum (1)
+        0x75, 0x01,                     //     Report Size (1)
+        0x95, 0x01,                     //     Report Count (1)
+        0x81, 0x02,                     //     Input (variable,absolute)
+        0x09, 0x30,                     //     Usage (Pressure)
+        0x25, 0x7F,                     //     Logical Maximum (127)
+        0x75, 0x07,                     //     Report Size (7)
+        0x95, 0x01,                     //     Report Count (1)
+        0x81, 0x02,                     //     Input (variable,absolute)
+        0x09, 0x51,                     //     Usage (Contact Identifier)
+        0x26, 0xFF, 0x00,               //     Logical Maximum (255)
+        0x75, 0x08,                     //     Report Size (8)
+        0x95, 0x01,                     //     Report Count (1)
+        0x81, 0x02,                     //     Input (variable,absolute)
+        0x05, 0x01,                     //     Usage Page (Generic Desktop)
         0x09, 0x30,                     //     Usage (X)
         0x09, 0x31,                     //     Usage (Y)
         0x26, 0xFF, 0x7F,               //     Logical Maximum (32767)
+        0x65, 0x00,                     //     Unit (None)  <-- probably needs real units?
         0x75, 0x10,                     //     Report Size (16)
+        0x95, 0x02,                     //     Report Count (2)
         0x81, 0x02,                     //     Input (variable,absolute)
         0xC0,                           //   End Collection
+        0x05, 0x0D,                     //   Usage Page (Digitizer)
+        0x27, 0xFF, 0xFF, 0, 0,         //   Logical Maximum (65535)
+        0x75, 0x10,                     //   Report Size (16)
+        0x95, 0x01,                     //   Report Count (1)
+        0x09, 0x56,                     //   Usage (Scan Time)
         0x81, 0x02,                     //   Input (variable,absolute)
+        0x09, 0x54,                     //   Usage (Contact Count)
         0x25, MULTITOUCH_FINGERS,       //   Logical Maximum (10)
         0x75, 0x08,                     //   Report Size (8)
         0x95, 0x01,                     //   Report Count (1)
@@ -1278,7 +1334,6 @@ static uint8_t config_descriptor[CONFIG_DESC_SIZE] = {
         MULTITOUCH_SIZE, 0,                     // wMaxPacketSize
         1,                                      // bInterval
 #endif // KEYMEDIA_INTERFACE
-
 };
 
 
@@ -1390,19 +1445,19 @@ const usb_descriptor_list_t usb_descriptor_list[] = {
 #ifdef MULTIJOY_INTERFACE
         #if MULTIJOY_COUNT > 0
                 {0x2200, (MULTIJOY_INTERFACE + 0), multijoy_report_desc, sizeof(multijoy_report_desc)},
-                {0x2100, (MULTIJOY_INTERFACE + 0), config_descriptor+MULTIJOY_HID_DESC_OFFSET, 9},
+                {0x2100, (MULTIJOY_INTERFACE + 0), config_descriptor+MULTIJOY_DESC_OFFSET, 9},
         #endif
         #if MULTIJOY_COUNT > 1
                 {0x2200, (MULTIJOY_INTERFACE + 1), multijoy_report_desc, sizeof(multijoy_report_desc)},
-                {0x2100, (MULTIJOY_INTERFACE + 1), config_descriptor+MULTIJOY_HID_DESC_OFFSET, 9},
+                {0x2100, (MULTIJOY_INTERFACE + 1), config_descriptor+MULTIJOY_DESC_OFFSET, 9},
         #endif
         #if MULTIJOY_COUNT > 2
                 {0x2200, (MULTIJOY_INTERFACE + 2), multijoy_report_desc, sizeof(multijoy_report_desc)},
-                {0x2100, (MULTIJOY_INTERFACE + 2), config_descriptor+MULTIJOY_HID_DESC_OFFSET, 9},
+                {0x2100, (MULTIJOY_INTERFACE + 2), config_descriptor+MULTIJOY_DESC_OFFSET, 9},
         #endif
         #if MULTIJOY_COUNT > 3
                 {0x2200, (MULTIJOY_INTERFACE + 3), multijoy_report_desc, sizeof(multijoy_report_desc)},
-                {0x2100, (MULTIJOY_INTERFACE + 3), config_descriptor+MULTIJOY_HID_DESC_OFFSET, 9},
+                {0x2100, (MULTIJOY_INTERFACE + 3), config_descriptor+MULTIJOY_DESC_OFFSET, 9},
         #endif
 #endif
 #ifdef RAWHID_INTERFACE

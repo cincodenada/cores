64bc1859c83b6b57eba7b99f0c8f0ed8b312daa0
Ell Bradshaw <ell@c9a.co>
2019-09-21 17:08:53 -0700

    Initial definition of gamepad as duplicate

--- teensy3/usb_desc.h
+++ teensy3/usb_desc.h
@@ -313,6 +313,26 @@ let me know?  http://forum.pjrc.com/forums/4-Suggestions-amp-Bug-Reports
   #define ENDPOINT15_CONFIG	ENDPOINT_TRANSMIT_ONLY
   #define ENDPOINT16_CONFIG	ENDPOINT_TRANSMIT_ONLY
 
+#elif defined(USB_GAMEPAD)
+  #define VENDOR_ID             0x16C0
+  #define PRODUCT_ID            0x0484
+  #define DEVICE_CLASS		0x01
+  #define DEVICE_SUBCLASS	0x00
+  #define DEVICE_PROTOCOL	0x00
+  #define MANUFACTURER_NAME     {'T','e','e','n','s','y','d','u','i','n','o'}
+  #define MANUFACTURER_NAME_LEN 11
+  #define PRODUCT_NAME          {'G','a','m','e','p','a','d','s'}
+  #define PRODUCT_NAME_LEN      8
+  #define EP0_SIZE		64
+  #define NUM_ENDPOINTS		1
+  #define NUM_USB_BUFFERS	10
+  #define NUM_INTERFACE		1
+  #define GAMEPAD_INTERFACE     0
+  #define GAMEPAD_ENDPOINT      1
+  #define GAMEPAD_SIZE          16
+  #define GAMEPAD_INTERVAL      1
+  #define ENDPOINT1_CONFIG	ENDPOINT_TRANSMIT_ONLY
+
 #elif defined(USB_TOUCHSCREEN)
   #define VENDOR_ID		0x16C0
   #define PRODUCT_ID		0x04D3
